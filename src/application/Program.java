1d573b30aa2e9d62db72e73d2dce72cc09147ff1
Vinicius Santos <c.viniciussantos@outlook.com>
2020-07-14 14:20:59 -0300

    findById implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,22 +1,17 @@
 package application;
 
-import java.util.Date;
-
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
-import model.entities.Department;
 import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 
-
-		Department obj = new Department(1, "Books");
-		
-		Seller seller = new Seller(21, "Vinicius", "carlos.vinicius@ccee.org.br", new Date(), 1600.0, obj);
 		
 		SellerDao sellerDao = DaoFactory.createSellerDao();
+		
+		Seller seller = sellerDao.findById(14);
 		
 		System.out.println(seller);
 
